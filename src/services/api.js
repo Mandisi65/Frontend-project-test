f55db5c3fafc20a261cffb4e9ff6909ec65dc14b
mandisi65 <mandisitshezi17@gmail.com>
2026-09-15 16:17:00 +0200

    Updating my API component

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "fa9296eeca1fe2f651946482efdc82a5";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+
+    return data.results;
+}
