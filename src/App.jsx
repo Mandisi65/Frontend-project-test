d575b6f19a2ea6ef7776f8dd9564928d3ead4033
mandisi65 <mandisitshezi17@gmail.com>
2026-09-25 20:05:27 +0200

    Concluded the addToFavorites functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './css/App.css'
 import { Routes, Route} from "react-router-dom"
 import Favorites from './Pages/Favorites';
 import NavBar from './Components/NavBar';
+import { MovieProvider } from './contexts/MovieContext';
 
 function App() {
 
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className='main-content'>
         <Routes>
@@ -15,7 +16,7 @@ function App() {
           <Route path='/Favorites' element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   )
 }
 
--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,16 +1,21 @@
 import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({movie}){
+    const {removeFromFavorites, isFavorite, addToFavorites} = useMovieContext();
+    const favorite = isFavorite(movie.id);
 
-    function onFavoriteClick(){
-        alert("Clicked!!");
+    function onFavoriteClick(e){
+        e.preventDefault();
+        if(favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-overlay">
-                    <button className= "favorite-button" onClick={onFavoriteClick}>💗</button>
+                    <button className={`favorite-btn ${favorite ? "Active" : ""}`} onClick={onFavoriteClick}>💗</button>
                 </div>
             </div>
             <div className="movie-info">
